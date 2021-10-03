b14c9d8783de817faafd063b1b27c46a372a57c2
John Muthua <caveomuthama@gmail.com>
2021-10-03 15:23:42 +0300

    Formated code with black

--- src/app.py
+++ src/app.py
@@ -12,26 +12,30 @@ from sqlalchemy.sql.expression import null
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-#Initializing flask application
+# Initializing flask application
 app = Flask(__name__)
 
-#Add SQLAlchemy
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+# Add SQLAlchemy
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    BASE_DIR, "db.sqlite3"
+)
 db = SQLAlchemy(app)
 
 
-#Add Marshmallow
+# Add Marshmallow
 ma = Marshmallow(app)
 
-#Create the API model (SQLAlchemy)
+# Create the API model (SQLAlchemy)
 class BookMarkModel(db.Model):
-    __tablename__ = 'bookmark'
+    __tablename__ = "bookmark"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False, unique=True)
     description = db.Column(db.String(255))
     url = db.Column(db.String(255), nullable=False, unique=True)
-    created_at = db.Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
+    created_at = db.Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=True
+    )
     updated_at = db.Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
 
     def __init__(self, title, url, description, created_at, updated_at):
@@ -41,90 +45,98 @@ class BookMarkModel(db.Model):
         self.created_at = created_at
         self.updated_at = updated_at
 
+
 db.create_all()
 db.session.commit()
 
-#Create schema with object validation (marshmallow)
+# Create schema with object validation (marshmallow)
 class BookMarkSchema(ma.Schema):
-    title = fields.Str(required = True, allow_none=False)
-    url = fields.URL(relative = True, require_tld=True, error="invalid url representation")
-    description = fields.String(required = False, allow_none= True)
-    created_at = fields.DateTime(required = False, allow_none= True)
-    updated_at = fields.DateTime(required = False, allow_none= True)
+    title = fields.Str(required=True, allow_none=False)
+    url = fields.URL(
+        relative=True, require_tld=True, error="invalid url representation"
+    )
+    description = fields.String(required=False, allow_none=True)
+    created_at = fields.DateTime(required=False, allow_none=True)
+    updated_at = fields.DateTime(required=False, allow_none=True)
 
     class Meta:
-        fields = ('id','title', 'description', 'url', 'created_at', 'updated_at')
+        fields = ("id", "title", "description", "url", "created_at", "updated_at")
 
 
 BookMark = BookMarkSchema()
-BookMarks = BookMarkSchema(many = True)
+BookMarks = BookMarkSchema(many=True)
 
 
-
-#API ROUTES 
-#Get all bookmarks
-@app.route('/bookmarks/', methods = ['GET'])
+# API ROUTES
+# Get all bookmarks
+@app.route("/bookmarks/", methods=["GET"])
 def book_marks():
     all_bookmarks = BookMarkModel.query.all()
     return jsonify(BookMarks.dump(all_bookmarks))
 
-#CREATE a bookmark
-@app.route('/bookmark/', methods=['POST'])
-def create_bookmark():
-    title = request.json['title']
-    description = request.json['description']
-    url = request.json['url']
 
-    #Validate the data from request before serialization
-    error = BookMark.validate({'title':title,'description':description, 'url':url})
-    if (error):
+# CREATE a bookmark
+@app.route("/bookmark/", methods=["POST"])
+def create_bookmark():
+    title = request.json["title"]
+    description = request.json["description"]
+    url = request.json["url"]
+
+    # Validate the data from request before serialization
+    error = BookMark.validate({"title": title, "description": description, "url": url})
+    if error:
         return jsonify(error)
-    
-    book_mark = BookMarkModel(title =title,description=description, url=url, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+
+    book_mark = BookMarkModel(
+        title=title,
+        description=description,
+        url=url,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now(),
+    )
     db.session.add(book_mark)
     db.session.commit()
     return BookMark.jsonify(book_mark)
 
 
-#READ a paticular bookmark
-@app.route('/bookmark/<int:id>/', methods=['GET'])
+# READ a paticular bookmark
+@app.route("/bookmark/<int:id>/", methods=["GET"])
 def read_bookmark(id):
     book_mark = BookMarkModel.query.get(id)
     return BookMark.jsonify(book_mark)
 
-#UPDATE a particular bookmark
-@app.route('/bookmark/<int:id>/', methods=['PUT'])
+
+# UPDATE a particular bookmark
+@app.route("/bookmark/<int:id>/", methods=["PUT"])
 def update_bookmark(id):
-    title = request.json['title']
-    description = request.json['description']
-    url = request.json['url']
+    title = request.json["title"]
+    description = request.json["description"]
+    url = request.json["url"]
 
     book_mark = BookMarkModel.query.get(id)
     book_mark.title = title
     book_mark.description = description
     book_mark.url = url
 
-    #Validate the data from request before serialization
-    error = BookMark.validate({'title':title,'description':description, 'url':url})
-    if (error):
+    # Validate the data from request before serialization
+    error = BookMark.validate({"title": title, "description": description, "url": url})
+    if error:
         return jsonify(error)
-        
+
     db.session.add(book_mark)
     db.session.commit()
     return BookMark.jsonify(book_mark)
 
-#DELETE a particular bookmark
-@app.route('/bookmark/<int:id>/', methods=['DELETE'])
+
+# DELETE a particular bookmark
+@app.route("/bookmark/<int:id>/", methods=["DELETE"])
 def delete_bookmark(id):
     book_mark = BookMarkModel.query.get(id)
     db.session.delete(book_mark)
     db.session.commit()
-    return jsonify({
-        'success':'True'
-    })
+    return jsonify({"success": "True"})
 
-#Serve the application
-if __name__ == '__main__':
+
+# Serve the application
+if __name__ == "__main__":
     app.run(debug=True)
-
-
--- tests/test__bookmark.py
+++ tests/test__bookmark.py
@@ -9,50 +9,56 @@ from src.app import app
 
 def generate_random_letters(num=50):
     word = string.ascii_letters
-    words = ''.join(random.choice(word) for i in range(num))
+    words = "".join(random.choice(word) for i in range(num))
     return words
+
+
 def generate_random_domain():
-    list = ['.com','.net','.org','.edu', '.info']
+    list = [".com", ".net", ".org", ".edu", ".info"]
     return random.choice(list)
+
 
 def generate_random_url():
     s1 = generate_random_letters(10)
     s2 = generate_random_domain()
-    s3 = 'http://' + s1 + s2
+    s3 = "http://" + s1 + s2
     return s3
 
-#TEST index page for 404
-@app.route('/', methods=['GET'])
+
+# TEST index page for 404
+@app.route("/", methods=["GET"])
 def test_index_page_gives_404():
-    res = app.test_client().get('/')
+    res = app.test_client().get("/")
     assert res.status_code == 500
 
 
-
-#Test if one can post data to the database
+# Test if one can post data to the database
 def test_post_one_bookmarks():
     my_data = {
-        'title':generate_random_letters(),
-        'description':generate_random_letters(100),
-        'url':generate_random_url()
+        "title": generate_random_letters(),
+        "description": generate_random_letters(100),
+        "url": generate_random_url(),
     }
-    res = app.test_client().post('/bookmark/',
-                            data=json.dumps(my_data),
-                            content_type ='application/json',
-                            )
+    res = app.test_client().post(
+        "/bookmark/",
+        data=json.dumps(my_data),
+        content_type="application/json",
+    )
     data = json.loads(res.get_data(as_text=True))
 
     assert res.status_code == 200
 
-#test if all bookmarks are returned 
+
+# test if all bookmarks are returned
 def test_get_all_bookmarks_route():
-    res = app.test_client().get('/bookmarks/')
+    res = app.test_client().get("/bookmarks/")
     data = json.loads(res.get_data(as_text=True))
     assert res.status_code == 200
 
-#test json data format is  are returned 
+
+# test json data format is  are returned
 def test_get_json_data_format_returns():
-    res = app.test_client().get('/bookmarks/')
+    res = app.test_client().get("/bookmarks/")
     data = json.loads(res.get_data(as_text=True))
     assert res.status_code == 200
-    assert res.headers['Content-Type'] == 'application/json'
+    assert res.headers["Content-Type"] == "application/json"
